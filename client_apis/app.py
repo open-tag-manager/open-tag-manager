015d1e3f80e80d018e6cb6368d6a40295359202e
Alexander Keith <akjjak18@gmail.com>
2020-01-24 23:46:29 +0900

    Add plugin api feature

--- client_apis/app.py
+++ client_apis/app.py
@@ -3,6 +3,9 @@ from chalicelib import authorizer, get_roles
 from chalicelib.container_routes import container_routes
 from chalicelib.swagger_doc_routes import swagger_doc_routes
 from chalicelib.stats_routes import stats_routes
+import importlib
+import json
+import os
 
 app = Chalice(app_name="open_tag_manager")
 app.debug = True
@@ -22,3 +25,8 @@ def organizations():
 app.register_blueprint(container_routes, url_prefix='/orgs/{org}/containers')
 app.register_blueprint(swagger_doc_routes, url_prefix='/orgs/{org}/containers/{name}/swagger_doc')
 app.register_blueprint(stats_routes, url_prefix='/orgs/{org}/containers/{name}/stats')
+
+plugins = json.loads(os.environ.get('OTM_PLUGINS'), encoding='utf-8')
+for plugin in plugins:
+    module = importlib.import_module('chalicelib.otmplugins.' + plugin['package'] + '.' + plugin['module'])
+    app.register_blueprint(module.app, url_prefix=plugin['urlPrefix'])
