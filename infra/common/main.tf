29193d802033061df1fb60798092c3e353e63f98
Alexander Keith <akjjak18@gmail.com>
2020-05-29 00:05:13 +0900

    Fix log bucket acl for cloudfront log

--- infra/common/main.tf
+++ infra/common/main.tf
@@ -29,10 +29,23 @@ resource "aws_s3_bucket_object" "otm_collect" {
   content_type = "text/html"
 }
 
+data "aws_canonical_user_id" "current" {}
+
 resource "aws_s3_bucket" "otm_collect_log" {
   bucket = "${terraform.workspace}-${var.aws_s3_bucket_prefix}-otm-collect-log"
-  acl = "private"
   tags = var.aws_resource_tags
+
+  grant {
+    id          = data.aws_canonical_user_id.current.id
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
+
+  grant {
+    id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
 }
 
 locals {
