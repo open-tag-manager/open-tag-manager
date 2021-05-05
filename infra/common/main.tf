11fbc334813f66f99c247c534af2267bcd9e5c92
Alexander Keith <akjjak18@gmail.com>
2021-05-05 09:08:02 +0900

    [infra] Fix fallback policy for client distoribution

--- infra/common/main.tf
+++ infra/common/main.tf
@@ -277,6 +277,20 @@ resource "aws_cloudfront_distribution" "otm_client_distribution" {
     }
   }
 
+  custom_error_response {
+    error_caching_min_ttl = 3600
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 3600
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   tags = var.aws_resource_tags
 }
 
